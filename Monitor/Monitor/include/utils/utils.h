400230ce3e1bb941481db6f4d36e7e8b26318c90
DieSnowK <1752351098@qq.com>
2024-07-06 19:19:03 +0800

    Utils Done

--- Monitor/Monitor/include/utils/utils.h
+++ Monitor/Monitor/include/utils/utils.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 #include <boost/chrono.hpp>
 
 namespace monitor
@@ -13,5 +18,24 @@ namespace monitor
             boost::chrono::duration<double> timeSecond = t1 - t2;
             return timeSecond.count();
         }
+
+        class ReadFile
+        {
+            explicit ReadFile(const std::string &name)
+                : _ifs(name)
+            {}
+
+            ~ReadFile()
+            {
+                _ifs.close();
+            }
+
+            bool ReadLine(std::vector<std::string> *args);
+            static std::vector<std::string> GetStatsLines(const std::string &statFile,
+                                                          const int lineCount);
+
+        private:
+            std::fstream _ifs;
+        };
     };
 } // end of namespace monitor
---
+++ Monitor/Monitor/src/utils/utils.cpp
@@ -0,0 +1,47 @@
+#include "utils.h"
+
+namespace monitor
+{
+    bool Utils::ReadFile::ReadLine(std::vector<std::string> *args)
+    {
+        std::string line;
+        
+        std::getline(_ifs, line);
+        if(_ifs.eof() || line.empty())
+        {
+            return false;
+        }
+
+        std::istringstream line_ss(line);
+        while(!line_ss.eof())
+        {
+            std::string word;
+            line_ss >> word;
+            args->push_back(word);
+        }
+
+        return true;
+    }
+
+    std::vector<std::string> Utils::ReadFile::GetStatsLines(const std::string &statFile,
+                                                                   const int lineCount)
+    {
+        std::vector<std::string> stats_lines;
+        std::ifstream buffer(statFile);
+
+        for (int i = 0; i < lineCount; ++i)
+        {
+            std::string line;
+
+            std::getline(buffer, line);
+            if(line.empty())
+            {
+                break;
+            }
+
+            stats_lines.push_back(line);
+        }
+
+        return stats_lines;
+    }
+}
