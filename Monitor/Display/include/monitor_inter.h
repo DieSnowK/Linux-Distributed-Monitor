033e042a27b1cc24a0d07cb1c981a91193a2a1e4
DieSnowK <1752351098@qq.com>
2024-07-10 01:15:27 +0800

    monitor_inter.h Done

---
+++ Monitor/Display/include/monitor_inter.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <QAbstractTableModel>
+#include <QObject>
+
+namespace monitor
+{
+    class MonitorInterModel : public QAbstractTableModel
+    {
+        Q_OBJECT
+    public:
+        explicit MonitorInterModel(QObject *parent = nullptr)
+            :QAbstractTableModel(parent) 
+        {}
+        virtual ~MonitorInterModel() {}
+
+        QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+        Qvariant headerData(int section, Qt::Orientation orientation, int role) const override;
+    };
+} // end of namespace monitor
